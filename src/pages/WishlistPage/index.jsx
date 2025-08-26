42f594cc49f65a45bef311ed62564371da6fd388
Safidjat <safidjat.isayeva.04@gmail.com>
2025-08-26 22:46:46 +0400

    clear

--- src/pages/WishlistPage/index.jsx
+++ src/pages/WishlistPage/index.jsx
@@ -6,10 +6,11 @@ import LoadingVerify from "../../components/LoadingVerify";
 import { useWishList } from "../../context/WishlistContext";
 import { useScrollY } from "../../context/ScrollRestoreContext";
 import { scrollTo } from "../../utilities/scroll";
+import { Button } from "@mui/material";
 
 function WishlistPage() {
     const{isLoading,isLoggedIn}=useAuth();
-    const {wishBasket}=useWishList();
+    const {wishBasket,clearWishes}=useWishList();
     const {handleScrollY,setScrollYinfo}=useScrollY();
 
     useLayoutEffect(() => {
@@ -26,7 +27,39 @@ function WishlistPage() {
         isLoading?<LoadingVerify />:
         !isLoggedIn?<Auth />:
         <div className="w-full max-[210px]:px-[16px] p-[60px] flex flex-col gap-[30px]">
-            <h1 className="text-[18px] min-[800px]:text-[25px] font-bold text-white">My List</h1>
+            <div className="w-full flex items-center justify-between">
+                <h1 className="text-[18px] min-[800px]:text-[25px] font-bold text-white">My List</h1>
+                {
+                    wishBasket.basket.length&&
+                    <Button
+                    onClick={clearWishes}
+                    variant="outlined" 
+                    sx={{
+                        '@media (max-width: 365px)': {
+                        width: '80px',
+                        fontSize: '10px',
+                        textAlign:'center',
+                        px:'5px'  
+                        },
+                        width: '150px',
+                        fontSize:'14px',
+                        fontWeight: '400',
+                        px: '15px',
+                        py: '5px',
+                        borderColor: 'white', 
+                        color: 'white',       
+                        borderRadius: '8px',
+                        '&:hover': {
+                        backgroundColor: 'white',
+                        color: '#030408',
+                        borderColor: 'white',
+                        },
+                    }}
+                    >
+                    CLEAR
+                    </Button>
+                }
+            </div>
             <div className="min-h-[567.6px]">
                 {
                     wishBasket.basket.length
